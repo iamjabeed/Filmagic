dd283b905712385499ba7baa8afc41bd89ec445b
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-04-09 16:49:55 +0530

    redux setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { fetchDataFromApi } from "./utils/api";
 
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./utils/homeSlice";
+
 const App = () => {
+  const dispatch = useDispatch();
+  const { url } = useSelector((store) => store.home);
+  console.log(url);
   useEffect(() => {
     apiTesting();
   }, []);
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
-      console.log(res);
+      // console.log(res);
+      dispatch(getApiConfiguration(res));
     });
-    // console.log(json);
   };
 
-  return <div>App</div>;
+  return <div>{url?.total_pages}</div>;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.scss'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.scss";
+import { store } from "./utils/store";
+import { Provider } from "react-redux";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
