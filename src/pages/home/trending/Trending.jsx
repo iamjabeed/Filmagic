a7176c14ca588eefe3ec86baff519b6ad90af54a
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-04-12 16:20:52 +0530

    cards and shimmer UI

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import Carousel from "../../../components/carousel/Carousel";
 
 import useFetch from "../../../hooks/useFetch";
 
@@ -21,6 +22,7 @@ const Trending = () => {
         <span className="carouselTitle">Trending</span>
         <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
+      <Carousel data={data?.results} loading={loading} />
     </div>
   );
 };
