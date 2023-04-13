2808be64c5a671a269ce4b2eaa57f2093df40903
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-04-13 20:55:05 +0530

    popup video through reactplayer

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -14,11 +14,14 @@ import PosterFallback from "../../../assets/no-poster.png";
 
 import { FcRating } from "react-icons/fc";
 
-import { PlayIcon } from "./PlayIcon";
-
 import startIcon from "../../../assets/star-fill.svg";
+import { PlayIcon } from "./PlayIcon";
+import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
+
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}`);
 
@@ -82,7 +85,11 @@ const DetailsBanner = ({ video, crew }) => {
                       {data?.vote_average.toFixed(1)}
                        <img src={startIcon} alt="startIcon" />
                       </span> */}
-                      <div className="playbtn">
+                      <div className="playbtn"
+                      onClick={()=>{setShow(true)
+                      setVideoId(video?.key)}
+                      }
+                      >
                         <PlayIcon />
                         <span className="text">Watch Trailer</span>
                       </div>
@@ -157,10 +164,15 @@ const DetailsBanner = ({ video, crew }) => {
                         </span>
                       </div>
                     )}
-
-                    
                   </div>
                 </div>
+
+                <VideoPopup
+                  show={show}
+                  setShow={setShow}
+                  videoId={videoId}
+                  setVideoId={setVideoId}
+                />
               </ContentWrapper>
             </React.Fragment>
           )}
