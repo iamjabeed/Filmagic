efcfe7e039335c5ec7faade7a8a6502402c538bd
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-04-17 23:35:34 +0530

    footer fixed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,39 +11,39 @@ const Footer = () => {
   return (
     <footer className="footer">
       <ContentWrapper>
-        <div className="description">
-          <div className="logo">
+          {/* <div className="logo">
             <img src={Logo} alt="Logo" />
-          </div>
+          </div> */}
           <div className="content">All right reserved &#169; 2023</div>
+       
+        <div className="infoText">Made with 💛 by Jabeed.</div>
+        <div className="socialIcons">
+          <a
+            href="https://instagram.com/iamjabeed_04"
+            target="_blank"
+            className="icon"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://twitter.com/iamlord04"
+            target="_blank"
+            className="icon"
+          >
+            <FaTwitter />
+          </a>
+          <a
+            href="https://linkedin.com/in/iamjabeed"
+            target="_blank"
+            className="icon"
+          >
+            <FaLinkedin />
+          </a>
         </div>
 
-        <div className="contactDeatils">
-          <div className="socialIcons">
-            <a
-              href="https://instagram.com/iamjabeed_04"
-              target="_blank"
-              className="icon"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://twitter.com/iamlord04"
-              target="_blank"
-              className="icon"
-            >
-              <FaTwitter />
-            </a>
-            <a
-              href="https://linkedin.com/in/iamjabeed"
-              target="_blank"
-              className="icon"
-            >
-              <FaLinkedin />
-            </a>
-          </div>
-          <div className="infoText">Made with 💛 by Jabeed.</div>
-        </div>
+        {/* <div className="contactDeatils">
+         
+        </div> */}
       </ContentWrapper>
     </footer>
   );
